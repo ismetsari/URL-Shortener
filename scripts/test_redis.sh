d557a93594d7fffb43cbed4a6cbf3689f7a67ed7
Ismet <41280907+ismetsari@users.noreply.github.com>
2025-05-13 02:29:06 +0300

    Update test_redis.sh

--- scripts/test_redis.sh
+++ scripts/test_redis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Configuration
-REDIS_HOST=${REDIS_HOST:-192.168.49.2}
+minikube_ip=$(minikube ip)
+REDIS_HOST=${REDIS_HOST:-$minikube_ip}
 REDIS_PORT=${REDIS_PORT:-30033}
-REDIS_PASSWORD=${REDIS_PASSWORD:-"fM3NRGz9b5"}
+REDIS_PASSWORD=$(kubectl get secret url-shortener-redis -o jsonpath="{.data.redis-password}" | base64 --decode)
 
 # Run test commands
 if [[ -n "$REDIS_PASSWORD" ]]; then
