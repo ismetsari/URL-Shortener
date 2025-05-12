9f977a76af1964f0845a9301d98cabc5814a4d92
Ismet <41280907+ismetsari@users.noreply.github.com>
2025-05-13 02:28:52 +0300

    Update test_postgres.sh

--- scripts/test_postgres.sh
+++ scripts/test_postgres.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Configuration
-PGHOST=${PGHOST:-192.168.49.2}
+minikube_ip=$(minikube ip)
+PGHOST=${PGHOST:-$minikube_ip}
 PGPORT=${PGPORT:-30032}
 PGUSER=${PGUSER:-postgres}
 PGDATABASE=${PGDATABASE:-urlshortener}
-PGPASSWORD=${PGPASSWORD:-postgres}
+PGPASSWORD=$(kubectl get secret postgres-credentials -o jsonpath="{.data.postgres-password}" | base64 --decode)
 
 # Run query with expanded output and no pager
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE --pset=pager=off <<EOF
